7543b45549f46bfb99ad87b37cf7445fc3eda202
keldonanatol <keldonanatole@gmail.com>
2022-05-14 23:22:57 -0400

    Update RecipeData.js

--- src/RecipeData.js
+++ src/RecipeData.js
@@ -18,6 +18,13 @@ const RecipeData = [
     ingredients: "3 ripe avocados. ¾ cup finely chopped Roma tomato. 2 serrano chiles very finely chopped (seeded and deveined). 3 heaping tablespoons of finely chopped onion. 3 tablespoons of minced cilantro. Salt to taste",
     preparation: "1. Remove the flesh of the avocados. 2. Mash the avocados with the back of a fork. 3. Add the other ingredients and incorporate evenly. 4. Add salt to taste."
   },
+  {
+    name: "Chicken Biriyani",
+    cuisine: "Nepali",
+    photo: "https://images.food52.com/3VTafrogqnexkhSLZ81-phWdqD4=/1008x672/filters:format(webp)/d1c35b2a-2b41-4a6c-ab04-7b81b95642bf--Spicy-Chicken-Biryani-recipe.jpg",
+    ingredients: "3 tablespoons plain yogurt(Indian curd). 1 1∕4 ",
+    preparation: "1. This recipe needs half kg chicken (1.1 lbs.). Make few slits on all the chicken pieces and add to a large bowl. Then add",
+  }
 ];
 
 export default RecipeData;
